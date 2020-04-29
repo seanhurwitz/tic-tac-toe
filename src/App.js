bc2d0c456b41dc6d5f25cec6b38705c2b0501baa
seanhurwitz <57170877+seanhurwitz@users.noreply.github.com>
2020-04-29 09:49:47 +0200

    uhoh

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    board: [
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""],
+    ],
+    xTurn: true,
+    turns: 0,
+  };
+  checkIfWon = () => {
+    for (let a = 0; a < 3; a++) {}
+  };
+  turnHandler = (rowIdx, colIdx) => {
+    const board = JSON.parse(JSON.stringify(this.state.board));
+    if (!board[rowIdx][colIdx]) {
+      board[rowIdx][colIdx] = this.state.xTurn ? "X" : "O";
+      this.checkIfWon();
+      this.setState((prevState) => ({
+        board,
+        xTurn: !prevState.xTurn,
+        turns: prevState.turns + 1,
+      }));
+    }
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1>TIC TAC TOE</h1>
+        <div className="Board">
+          {this.state.board.map((row, rowIdx) => (
+            <div className="Row" key={rowIdx}>
+              {row.map((cell, colIdx) => (
+                <div
+                  key={`${rowIdx + 1},${colIdx + 1}`}
+                  className="Cell"
+                  style={{
+                    backgroundColor: !cell
+                      ? "none"
+                      : cell === "X"
+                      ? "blue"
+                      : "orange",
+                  }}
+                  onClick={() => this.turnHandler(rowIdx, colIdx)}
+                >
+                  {cell}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
